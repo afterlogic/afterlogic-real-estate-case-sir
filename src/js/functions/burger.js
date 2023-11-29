ef6e23f5a7dff9d31f2a702159e67bb87ed6b0bd
Vitalii Stepnov <97784759+Vetosy@users.noreply.github.com>
2023-11-29 23:25:17 +0300

    fix#1

--- src/js/functions/burger.js
+++ src/js/functions/burger.js
@@ -6,26 +6,34 @@ import { enableScroll } from '../functions/enable-scroll';
   const menu = document?.querySelector('[data-menu]');
   const menuItems = document?.querySelectorAll('[data-menu-item]');
   const overlay = document?.querySelector('[data-menu-overlay]');
+  const closeBtn = document?.querySelector('[data-menu-close]');
 
   burger?.addEventListener('click', (e) => {
     burger?.classList.toggle('burger--active');
     menu?.classList.toggle('menu--active');
-    overlay.classList.toggle('overlay--active')
 
     if (menu?.classList.contains('menu--active')) {
       burger?.setAttribute('aria-expanded', 'true');
-      burger?.setAttribute('aria-label', 'Close menu');
+      burger?.setAttribute('aria-label', 'Закрыть меню');
       disableScroll();
     } else {
       burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Open menu');
+      burger?.setAttribute('aria-label', 'Открыть меню');
       enableScroll();
     }
   });
 
+  closeBtn?.addEventListener('click', () => {
+    burger?.setAttribute('aria-expanded', 'false');
+    burger?.setAttribute('aria-label', 'Открыть меню');
+    burger.classList.remove('burger--active');
+    menu.classList.remove('menu--active');
+    enableScroll();
+  });
+
   overlay?.addEventListener('click', () => {
     burger?.setAttribute('aria-expanded', 'false');
-    burger?.setAttribute('aria-label', 'Open menu');
+    burger?.setAttribute('aria-label', 'Открыть меню');
     burger.classList.remove('burger--active');
     menu.classList.remove('menu--active');
     enableScroll();
@@ -34,12 +42,10 @@ import { enableScroll } from '../functions/enable-scroll';
   menuItems?.forEach(el => {
     el.addEventListener('click', () => {
       burger?.setAttribute('aria-expanded', 'false');
-      burger?.setAttribute('aria-label', 'Open menu');
+      burger?.setAttribute('aria-label', 'Открыть меню');
       burger.classList.remove('burger--active');
       menu.classList.remove('menu--active');
       enableScroll();
-      overlay.classList.remove('overlay--active');
     });
   });
-
 })();
